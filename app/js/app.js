e30d8c2464cc6cae237ec4aa10704af2b29e0813
Elliott Williams <e@elliottwillia.ms>
2014-03-24 09:57:05 -0400

    Moved bower dependencies to app/vendor.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('modulusOne', [
   'ngRoute',
-  'ngResource',
   'restangular',
   'modulusOne.filters',
   'modulusOne.services',
