7fc0a18a9c0930277a9cc35f99c7121517438e8c
Elliott Williams <e@elliottwillia.ms>
2014-05-29 14:33:00 -0400

    Make defaults saner.

--- config/modulusui.conf.js
+++ config/modulusui.conf.js
@@ -5,10 +5,10 @@ module.exports = {
 
     // Base URL of the lcoation where Modulus Core is running. Do not include
     // `/api` in the base URL.
-    baseUrl: 'http://localhost:8080',
+    baseUrl: '/modulus',
 
     // Set to `true` to disable writable actions on the Modulus API, such as
     // uploading releases or editing modules.
-    readOnly: true
+    readOnly: false
   }
 }
