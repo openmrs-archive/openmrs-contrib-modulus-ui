bc9a1acfd2b119b8de17ba667e1523470b816ebe
Richard Szczerba <richardszczerba@gmail.com>
2015-01-16 22:47:14 +0000

    Update modulusui.conf.js

--- config/modulusui.conf.js
+++ config/modulusui.conf.js
@@ -5,19 +5,19 @@ module.exports = {
 
     // Base URL of the lcoation where Modulus Core is running. Do not include
     // `/api` in the base URL.
-    baseUrl: 'http://localhost:8080',
+    baseUrl: '/modulus',
 
     // Set to `true` to disable writable actions on the Modulus API, such as
     // uploading releases or editing modules.
     readOnly: false
   },
 
-  // URL where this instance of Modulus UI will be publicly accessible  
-  appUrl: 'http://localhost:8083',
+  // URL where this instance of Modulus UI will be publicly accessible  
+  appUrl: 'http://localhost:3001',
 
   // OAuth Client credentials
   auth: {
     authenticateUrl: 'http://localhost:8080/login?provider=openmrsid',
-    clientId: 'd3eb9260-7f5b-11e4-9b8d-0fb70cf652f2'
+    clientId: '8fa0753531217077ab449c37a4d0bd5b'
   }
 }
